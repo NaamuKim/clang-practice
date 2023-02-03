f5d1f82ed8f527742b14c6f9feab50ca8c358113
Jiwon Kim <lost731@naver.com>
2023-02-03 23:22:06 +0900

    Add: 선택정렬 버블정렬 구현

---
+++ 09/09-02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+    int aList[5] = {30, 40, 10, 50,20};
+    int i = 0, j = 0, nTmp = 0;
+
+    for(i =0; i<4; i++){
+        for(j=0; j<4; j++){
+            if(aList[j+1]<aList[j]){
+                // 바꿔줘야함
+                nTmp=aList[j+1];
+                aList[j+1]=aList[j];
+                aList[j]=nTmp;
+            }
+        }
+    }
+
+    for(i =0; i<5; ++i) printf("%d\t",aList[i]);
+    putchar('\n');
+    return 0;
+}
