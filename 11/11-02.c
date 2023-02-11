d6a84aa7788d6c4438cf774308546d32b625af66
Jiwon Kim <lost731@naver.com>
2023-02-11 23:10:05 +0900

    Add: 배열을 통해 printf 안에서 연산자 동작 이해

---
+++ 11/11-02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char szBuffer[32] = {"You are a girl."};
+    char *pszData = szBuffer + 4;
+
+    printf("%c\n", szBuffer[0]);
+    printf("%c\n", pszData[0]);
+    // %c이므로 해당 주소에 해당하는 
+    printf("%c\n", pszData[6]);
+
+    printf("%s\n", szBuffer + 4);
+    printf("%s\n", pszData);
+    // %s이므로 포인터 뒤에 있는 문자열 나옴
+    printf("%s\n", pszData + 4);
+    
+    return 0;
+
+    
+}
